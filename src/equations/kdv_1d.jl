50b5dd3d01f7dcafb7c18e2291437bb8009dd71f
Collin Wittenstein <cwittens@students.uni-mainz.de>
2025-05-15 17:34:09 +0200

    added documentation

--- src/equations/kdv_1d.jl
+++ src/equations/kdv_1d.jl
@@ -1,6 +1,6 @@
 @doc raw"""
-    KdVEquation1D(; gravity, D = 1.0, eta0 = 0.0)
-
+KdVEquation1D(; gravity, D = 1.0, eta0 = 0.0)
+KdV (Korteweg-de Vries) equation in one spatial dimension.
 The equation is given by
 ```math
 \begin{aligned}
@@ -8,13 +8,35 @@ The equation is given by
 \end{aligned}
 ```
 
-and the Semi is:
+The unknown quantity of the KdV equation is the total water height ``\eta``.
+The gravitational acceleration `gravity` is denoted by ``g`` and the constant bottom topography (bathymetry) ``b = \eta_0 - D``,
+where \eta_0 is the constant still-water surface and ``D`` the still-water depth. The water height above
+the bathymetry is therefore given by ``h = \eta - \eta_0 + D``. The KdV equation is only implemented for ``\eta_0 = 0``.
 
-taken from:
+The equations only support a flat bathymetry.
 
-...
+The KdV equation is first introduced by Joseph Valentin Boussinesq (1877) and rediscovered by Diederik Korteweg and Gustav de Vries in 1895.
 
-Hier noch ganz viel Documentation...
+The semidiscretization implemented here is a modification of the one proposed by Biswas, Ketcheson, Ranocha and Schütz (2025) for the non-dimensionalized KdV equation ``u_t+ u u_x + u_{x x x} = 0.``
+
+The semidiscretization looks the following:
+```math
+\begin{aligned}
+  \eta_t+\sqrt{g D} D^{x}\eta+ 1 / 2 \sqrt{g / D} \eta D^{x} \eta +  1 / 2 \sqrt{g / D} D^{x} \eta^2 +1 / 6 \sqrt{g D} D^2 {D^{x}}^3\eta $= 0.
+\end{aligned}
+```
+where ``D^{x}`` is the first derivative operator and ``D`` the still-water depth.
+
+It conserves
+- the total water mass (integral of ``\eta``) as a linear invariant
+and if upwind operators are used for the third derivative (``{D^{x}}^3 = D^{x}_{+} D^{x} D^{x}_{-}``), it also conserves
+- the energy (integral of ``1/2\eta^2``) 
+
+for periodic boundary conditions.
+
+- Abhijit Biswas, David I. Ketcheson, Hendrik Ranocha and Jochen Schütz (2025)
+  Traveling-Wave Solutions and Structure-Preserving Numerical Methods for a Hyperbolic Approximation of the Korteweg-de Vries Equation
+  [J Sci Comput 103, 90 (2025)](https://doi.org/10.1007/s10915-025-02898-x)
 """
 struct KdVEquation1D{RealT <: Real} <: AbstractKdVEquation{1, 1}
     gravity::RealT # gravitational acceleration
@@ -35,10 +57,6 @@ A traveling-wave solution used for convergence tests in a periodic domain, here 
 function initial_condition_convergence_test(x, t, equations::KdVEquation1D, mesh)
     g = gravity(equations)
     D = equations.D
-
-    # c = 1.5 * sqrt(g * D)
-    # A = 2*D * (c - sqrt(g * D)) / sqrt(g / D)
-
     A = 1.0 # amplitude (free parameter)
     K = 1 / 2 * sqrt(3 * A / D^3)
     c = (sqrt(g * D) + A * sqrt(g * D) / (2 * D))
